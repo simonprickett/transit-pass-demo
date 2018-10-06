1d491caca71acd5b63f153f84e6d124cd3c93f6a
Simon Prickett <simon.prickett@zesty.io>
2018-10-06 16:30:00 -0700

    Basic workflow for entry gate.

--- entry-gate/entry-gate.py
+++ entry-gate/entry-gate.py
@@ -1,5 +1,10 @@
 import os
 import redis
+import time
+
+PASS_TYPE_SINGLE_USE = 'SINGLE_USE'
+PASS_TYPE_TWO_HOUR = 'TWO_HOUR'
+PASS_TYPE_TEN_TRIP = 'TEN_TRIP'
 
 # Connect to Redis Instance
 r = redis.Redis(
@@ -7,3 +12,34 @@ r = redis.Redis(
     port = os.environ['TRANSIT_PASS_DEMO_REDIS_PORT'],
     password= os.environ['TRANSIT_PASS_DEMO_REDIS_PASSWORD']
 )
+
+def waitForCard():
+    return input('Card serial number: ')
+
+def getPassForCard(cardSerialNumber):
+    return r.hgetall(cardSerialNumber)
+
+def updatePass(cardSerialNumber, cardPass):
+    # TODO will depend on pass type...
+    return
+
+while(True):
+    # Set the light to red
+    print("Light: Red")
+
+    # Wait for a card to be presented
+    cardSerialNumber = waitForCard()
+
+    # Does this card have a pass associated with it?
+    cardPass = getPassForCard(cardSerialNumber)
+
+    if (cardPass):
+        # Update this card's pass
+        updatePass(cardSerialNumber, cardPass)
+
+        print("Light: Solid Green")
+        time.sleep(5)
+    else:
+        print("Light: Flashing Red")
+        time.sleep(3)
+
