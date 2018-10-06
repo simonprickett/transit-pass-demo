9a2bc165e14d4c45a076fc7f87e8558b30e391d9
Simon Prickett <simon.prickett@zesty.io>
2018-10-06 12:43:12 -0700

    Adds basic pass generation functions.

--- sales-terminal/sales-terminal.py
+++ sales-terminal/sales-terminal.py
@@ -7,3 +7,23 @@ r = redis.Redis(
     port = os.environ['TRANSIT_PASS_DEMO_REDIS_PORT'],
     password= os.environ['TRANSIT_PASS_DEMO_REDIS_PASSWORD']
 )
+
+def generateSingleUsePass():
+    # Use once, never expires.
+    return {
+        'passType': 'SINGLE_USE'
+    }
+
+def generateTwoHourPass():
+    # Use an unlimited amount of times within two hours of first use.
+    return {
+        'passType': 'TWO_HOUR'
+    }
+
+def generateTenTripPass():
+    # Use ten times, never expires.
+    return {
+        'passType': 'TEN_TRIP',
+        'tripsRemaining': 10
+    }
+
