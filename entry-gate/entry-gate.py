9aaf7ae2619ad38f3582e77cc26e32dc517b9717
Simon Prickett <simon.prickett@zesty.io>
2018-10-06 20:05:08 -0700

    Adds most of the entry gate logic.

--- entry-gate/entry-gate.py
+++ entry-gate/entry-gate.py
@@ -10,7 +10,8 @@ PASS_TYPE_TEN_TRIP = 'TEN_TRIP'
 r = redis.Redis(
     host = os.environ['TRANSIT_PASS_DEMO_REDIS_HOST'],
     port = os.environ['TRANSIT_PASS_DEMO_REDIS_PORT'],
-    password= os.environ['TRANSIT_PASS_DEMO_REDIS_PASSWORD']
+    password = os.environ['TRANSIT_PASS_DEMO_REDIS_PASSWORD'],
+    decode_responses = True
 )
 
 def waitForCard():
@@ -20,12 +21,45 @@ def getPassForCard(cardSerialNumber):
     return r.hgetall(cardSerialNumber)
 
 def updatePass(cardSerialNumber, cardPass):
-    # TODO will depend on pass type...
-    return
+    print(cardPass)
+    passType = cardPass.get('passType')
+
+    print(passType)
+
+    if (passType == PASS_TYPE_SINGLE_USE):
+        # This is the only use allowed for this pass so delete it from Redis
+        r.delete(cardSerialNumber)
+
+        # Report this pass was used
+        print('Single use pass used and deleted.')
+    elif (passType == PASS_TYPE_TWO_HOUR):
+        # Check if this is first use and start expiring the pass if so...
+        # Nothing to do except report this pass was used for a journey
+        print ('Two hour pass used.')
+        # TODO report remaining time?
+    else:
+        # Ten trip pass, delete one from the number of remaining trips, delete from Redis if 0
+        tripsRemaining = cardPass.get('tripsRemaining')
+
+        # Note tripsRemaining comes back as a String
+        if (tripsRemaining == '1'):
+            # Final trip for this pass, so delete this key
+            print('This ten trip pass has no more trips remaining, delete it.')
+            r.delete(cardSerialNumber)
+
+            # Report that this pass used up all of its trips
+        else:
+            # TODO hincr by -1 this field
+            r.hincrby(cardSerialNumber, 'tripsRemaining', -1)
+
+            newTripsRemaining = (int(tripsRemaining) - 1)
+
+            print('This ten trip pass has ' + str(newTripsRemaining) + ' more trips remaining.')
+            # Report that this pass was used and has trips remaining
 
 while(True):
     # Set the light to red
-    print("Light: Red")
+    print('Light: Red')
 
     # Wait for a card to be presented
     cardSerialNumber = waitForCard()
@@ -37,9 +71,9 @@ while(True):
         # Update this card's pass
         updatePass(cardSerialNumber, cardPass)
 
-        print("Light: Solid Green")
+        print('Light: Solid Green')
         time.sleep(5)
     else:
-        print("Light: Flashing Red")
+        # TODO Send a pubsub for invalid use of card...
+        print('Light: Flashing Red')
         time.sleep(3)
-
