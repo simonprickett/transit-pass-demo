806543477aeb389dc311f8d28f518cd742daa48e
Simon Prickett <simon.prickett@zesty.io>
2018-10-07 19:07:45 -0700

    Finalized event listeners.

--- system-monitor/server.js
+++ system-monitor/server.js
@@ -26,10 +26,6 @@ r.on('pass-issued:*', (msg) => {
   console.log(chalk.yellow(`Card ${msg.cardSerialNumber} was issued a ${decodePassType(msg.pass.passType)} pass.`))
 })
 
-r.on(`pass-issued:${PASS_TYPE_TEN_TRIP}`, (msg) => {
-  console.log('This would be an extra action only for the ten trip pass...')  
-})
-
 r.on(`pass-activated:${PASS_TYPE_TWO_HOUR}`, (msg) => {
   console.log(chalk.magenta(`Card ${msg.cardSerialNumber} two hour pass activated.`))
 })
@@ -37,7 +33,10 @@ r.on(`pass-activated:${PASS_TYPE_TWO_HOUR}`, (msg) => {
 r.on('pass-used:*', (msg) => {
   // some type of pass used
   console.log(chalk.blue(`Card ${msg.cardSerialNumber} started a journey.`))
-  console.log(msg)
+})
+
+r.on(`pass-used:${PASS_TYPE_TEN_TRIP}`, (msg) => {
+  console.log(chalk.yellow(`Card ${msg.cardSerialNumber} has ${msg.remainingTrips} of 10 trips remaining.`))
 })
 
 r.on('pass-denied:*', (msg) => {
